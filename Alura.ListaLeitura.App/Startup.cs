69406745cf0a438279eb639fb827614d2aee8a7a
Gustavo Habib <46494399+gushabib@users.noreply.github.com>
2022-12-14 20:22:14 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    internal class Startup
+    {
+        public void Configure(IApplicationBuilder app) 
+        {
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento (HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+                { "/Livros/ParaLer", _repo.ParaLer.ToString() },
+                { "/Livros/Lendo", _repo.Lendo.ToString() },
+                { "/Livros/Lidos", _repo.Lidos.ToString() }
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("Caminho Inexistente!");
+        }
+    }
+}
