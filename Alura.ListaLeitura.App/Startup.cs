b8442d8ea75e1c05db47780508f5791527e6b850
Gustavo Habib <46494399+gushabib@users.noreply.github.com>
2022-12-16 12:16:20 -0300

    aplicação web abrindo arquivo CSV - paraLer

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,10 +1,23 @@
-﻿namespace Alura.ListaLeitura.App
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
 {
     internal class Startup
     {
-        public void Configure() 
+        public void Configure(IApplicationBuilder app) 
         {
+            app.Run(LivrosParaLer);
 
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
         }
     }
 }
